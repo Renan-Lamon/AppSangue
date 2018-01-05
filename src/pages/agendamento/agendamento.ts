b4d0119ad9652d472278069e867bc632caee9d76
Renan-Lamon <renan-lamon@hotmail.com>
2018-01-05 13:30:18 -0200

    agendamento

--- src/pages/agendamento/agendamento.ts
+++ src/pages/agendamento/agendamento.ts
@@ -7,8 +7,23 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'agendamento.html'
 })
 export class AgendamentoPage {
-
+  campanhas: Array<{ titulo: string, sangue: string, hemocentro: string, criador: string, alvo: string, data: string, quantidade: number, cod: number }>;
+  grupos: Array<{ nome: string, campanha: any, quantidade: number, cod: number }>;
+  
   constructor(public navCtrl: NavController) {
+    this.campanhas = [
+      { titulo: 'Meu amigo precisa de você!', sangue: 'O+', hemocentro: 'Hospital Evangélico', criador: 'Juca da Silva', alvo: 'Juquinha Bizerra', data: '01/02/2018', quantidade: 5, cod: 1},
+      { titulo: 'Meu amigo precisa de você!2', sangue: 'O+', hemocentro: 'Hospital Evangélico', criador: 'Juca da Silva', alvo: 'Juquinha Bizerra', data: '01/02/2018', quantidade: 5, cod: 2},
+      { titulo: 'Meu amigo precisa de você!3', sangue: 'O+', hemocentro: 'Hospital Evangélico', criador: 'Juca da Silva', alvo: 'Juquinha Bizerra', data: '01/02/2018', quantidade: 5, cod: 3},
+      { titulo: 'Meu amigo precisa de você!4', sangue: 'O+', hemocentro: 'Hospital Evangélico', criador: 'Juca da Silva', alvo: 'Juquinha Bizerra', data: '01/02/2018', quantidade: 5, cod: 4}
+    ];
+    
+    this.grupos = [
+      { nome: 'Seila1', campanha:'',quantidade: 5, cod: 1},
+      { nome: 'Seila2', campanha:'',quantidade: 5, cod: 2},
+      { nome: 'Seila3', campanha:'',quantidade: 5, cod: 3}
+    ];
+
 
   }
 
--- src/pages/campanha/campanha.ts
+++ src/pages/campanha/campanha.ts
@@ -7,14 +7,14 @@ import {criarCampanhaPage} from '../criarCampanha/criarCampanha';
   templateUrl: 'campanha.html'
 })
 export class CampanhaPage {
-  campanhas: Array<{ titulo: string, sangue: string, hemocentro: string, criador: string, alvo: string, data: string, quantidade: number }>;
+  campanhas: Array<{ titulo: string, sangue: string, hemocentro: string, criador: string, alvo: string, data: string, quantidade: number, id: number }>;
 
   constructor(public navCtrl: NavController) {
     this.campanhas = [
-      { titulo: 'Meu amigo precisa de você!', sangue: 'O+', hemocentro: 'Hospital Evangélico', criador: 'Juca da Silva', alvo: 'Juquinha Bizerra', data: '01/02/2018', quantidade: 5 },
-      { titulo: 'Meu amigo precisa de você!', sangue: 'O+', hemocentro: 'Hospital Evangélico', criador: 'Juca da Silva', alvo: 'Juquinha Bizerra', data: '01/02/2018', quantidade: 5 },
-      { titulo: 'Meu amigo precisa de você!', sangue: 'O+', hemocentro: 'Hospital Evangélico', criador: 'Juca da Silva', alvo: 'Juquinha Bizerra', data: '01/02/2018', quantidade: 5 },
-      { titulo: 'Meu amigo precisa de você!', sangue: 'O+', hemocentro: 'Hospital Evangélico', criador: 'Juca da Silva', alvo: 'Juquinha Bizerra', data: '01/02/2018', quantidade: 5 }
+      { titulo: 'Meu amigo precisa de você!', sangue: 'O+', hemocentro: 'Hospital Evangélico', criador: 'Juca da Silva', alvo: 'Juquinha Bizerra', data: '01/02/2018', quantidade: 5, id: 1},
+      { titulo: 'Meu amigo precisa de você!', sangue: 'O+', hemocentro: 'Hospital Evangélico', criador: 'Juca da Silva', alvo: 'Juquinha Bizerra', data: '01/02/2018', quantidade: 5, id: 2},
+      { titulo: 'Meu amigo precisa de você!', sangue: 'O+', hemocentro: 'Hospital Evangélico', criador: 'Juca da Silva', alvo: 'Juquinha Bizerra', data: '01/02/2018', quantidade: 5, id: 3},
+      { titulo: 'Meu amigo precisa de você!', sangue: 'O+', hemocentro: 'Hospital Evangélico', criador: 'Juca da Silva', alvo: 'Juquinha Bizerra', data: '01/02/2018', quantidade: 5, id: 4}
 
     ];
     
