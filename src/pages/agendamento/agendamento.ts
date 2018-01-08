eb08499bc956e31aaba8e210c0c459496092cbb3
Renan-Lamon <renan-lamon@hotmail.com>
2018-01-08 11:21:59 -0200

    mudanças na hora e data

--- src/pages/agendamento/agendamento.ts
+++ src/pages/agendamento/agendamento.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { GrupoPage } from '../grupo/grupo';
+import { Time } from '@angular/common/src/i18n/locale_data_api';
 
 
 @Component({
@@ -9,8 +10,14 @@ import { GrupoPage } from '../grupo/grupo';
 })
 export class AgendamentoPage {
   campanhas: Array<{ titulo: string, sangue: string, hemocentro: string, criador: string, alvo: string, data: string, quantidade: number, cod: number }>;
-  grupos: Array<{ nome: string, campanha: any, quantidade: number, cod: number }>;
-  
+  grupos: Array<{ nome: string, campanha: any, quantidade: number, privado:boolean,data:string, horario:string, cod: number }>;
+  grupoSelecionado: string;
+  grupoSelecionadoData: string;
+  grupoSelecionadoHorario: string;
+
+  grupo:string='nenhum';
+  campanha:string='nenhuma';
+
   constructor(public navCtrl: NavController) {
     this.campanhas = [
       { titulo: 'Meu amigo precisa de você!', sangue: 'O+', hemocentro: 'Hospital Evangélico', criador: 'Juca da Silva', alvo: 'Juquinha Bizerra', data: '01/02/2018', quantidade: 5, cod: 1},
@@ -18,11 +25,14 @@ export class AgendamentoPage {
       { titulo: 'Meu amigo precisa de você!3', sangue: 'O+', hemocentro: 'Hospital Evangélico', criador: 'Juca da Silva', alvo: 'Juquinha Bizerra', data: '01/02/2018', quantidade: 5, cod: 3},
       { titulo: 'Meu amigo precisa de você!4', sangue: 'O+', hemocentro: 'Hospital Evangélico', criador: 'Juca da Silva', alvo: 'Juquinha Bizerra', data: '01/02/2018', quantidade: 5, cod: 4}
     ];
+    this.grupoSelecionado='vazio';
+    this.grupoSelecionadoHorario='vazio';
+    this.grupoSelecionadoData='vazio';
     
     this.grupos = [
-      { nome: 'Seila1', campanha:'',quantidade: 5, cod: 1},
-      { nome: 'Seila2', campanha:'',quantidade: 5, cod: 2},
-      { nome: 'Seila3', campanha:'',quantidade: 5, cod: 3}
+      { nome: 'Seila1', campanha:'',quantidade: 5,privado:true,data:'2015-08-01',horario:'09:32', cod: 1},
+      { nome: 'Seila2', campanha:'',quantidade: 5,privado:false,data:'2019-08-01',horario:'09:32', cod: 2},
+      { nome: 'Seila3', campanha:'',quantidade: 5,privado:false,data:'2019-08-01',horario:'09:32', cod: 3}
     ];
 
 
@@ -30,6 +40,16 @@ export class AgendamentoPage {
 
   chamarGrupo(){
     this.navCtrl.push(GrupoPage);
+  }
+  grupoSelecionadoSet(g:any){
+    this.grupoSelecionado= g.nome;
+    this.grupoSelecionadoData= g.data;
+    this.grupoSelecionadoHorario= g.horario;
+  }
+  grupoSelecionadoNenhum(){
+    this.grupoSelecionado= 'vazio';
+    this.grupoSelecionadoData= 'vazio';
+    this.grupoSelecionadoHorario= 'vazio';
   }
 
 }
