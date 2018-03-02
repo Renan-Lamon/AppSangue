f0ccb039346b7e02b56afaaa9d6527411c65bc63
Renan-Lamon <renan-lamon@hotmail.com>
2018-03-02 09:10:40 -0300

    tela criarCampanha DESKTOP

--- src/pages/criarCampanha/criarCampanha.ts
+++ src/pages/criarCampanha/criarCampanha.ts
@@ -7,16 +7,41 @@ import { HemocentroPage } from '../hemocentro/hemocentro';
 import { InformacaoPage } from '../informacao/informacao';
 import { HomePage } from '../home/home';
 import { RegistrarDoacaoPage } from '../registrarDoacao/registrarDoacao';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 
 @Component({
   selector: 'page-criarCampanha',
   templateUrl: 'criarCampanha.html'
 })
 export class criarCampanhaPage {
-
-  constructor(public navCtrl: NavController) {
+  campanha= {};
+  conta={};
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
 
   }
+
+  criarCampanha() {
+    this.mensagemConfirmacao();
+
+  }
+
+  mensagemConfirmacao() {
+    let alert = this.alertCtrl.create({
+      title: 'Campanha criada com sucesso!',
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => {
+            this.navCtrl.setRoot(HomePage);
+          }
+        }
+
+      ]
+    });
+    alert.present();
+    return alert;
+  }
+
 
   chamarRegistrarDoacao(){
     this.navCtrl.setRoot(RegistrarDoacaoPage);
